d3696e0cb8b02cd1f00e2f4657b99cda1d05c5ce
Niklas Marion <niklas.marion@gmx.de>
2021-01-06 16:28:50 +0000

    feat: read file content in new method

--- src/ueb06/LinkFilter.java
+++ src/ueb06/LinkFilter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,25 +20,34 @@ public class LinkFilter {
    *
    * @param input the text to filter
    */
-  public void filterLinks(String input) {
+  public static void filterLinks(String input) {
     final Matcher pageMatcher = PATTERN.matcher(input);
     while (pageMatcher.find()) {
       System.out.println(pageMatcher.group(1) + ":" + pageMatcher.group(2));
     }
   }
 
-  /** @param args the files to filter */
-  public static void main(String... args) {
-    final LinkFilter linkFilter = new LinkFilter();
-    for (String filename : args) {
-      final File file = new File(filename);
-      if (file.exists()) {
-        try {
-          linkFilter.filterLinks(Files.readString(Paths.get(file.getAbsolutePath())));
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
+  /**
+   * @param filename the path to the file
+   * @return the content of the file
+   */
+  public static String getFileContent(String filename) {
+    final File file = new File(filename);
+    if (file.exists()) {
+      try {
+        return Files.readString(Paths.get(file.getAbsolutePath()));
+      } catch (IOException e) {
+        e.printStackTrace();
       }
     }
+    return null;
+  }
+
+  /** @param args the files to filter */
+  public static void main(String... args) {
+    Arrays.asList(args).stream()
+        .map(m -> getFileContent(m))
+        .filter(input -> input != null)
+        .forEach(input -> filterLinks(input));
   }
 }
