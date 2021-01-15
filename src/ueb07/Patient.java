7dfdd7926b222541e96598422a2b160d57890539
Niklas Marion <niklas.marion@gmx.de>
2021-01-15 19:06:40 +0000

    fix: removing element from list

--- src/ueb07/Patient.java
+++ src/ueb07/Patient.java
@@ -3,25 +3,38 @@ package ueb07;
 public class Patient {
 
   private final int number;
-  private final String name;
+  private String name;
+  private static final int MIN = 1000;
+  private static final int MAX = 9999;
 
   /**
-   * @param number
-   * @param name
+   * Konstruktor für den Patient Überprüft, ob die ID vierstellig ist.
+   *
+   * @param number die Nummer des Patienten
+   * @param name der Name des Patienten
    */
   public Patient(int number, String name) {
+    if (number < MIN && number > MAX) {
+      throw new IllegalArgumentException("Die Patientennummer muss vierstellig sein");
+    }
     this.number = number;
-    this.name = name;
+    setName(name);
   }
 
-  /** @return the number */
   public int getNumber() {
     return number;
   }
 
-  /** @return the name */
   public String getName() {
     return name;
+  }
+
+  public void setName(String name) {
+    if (name == null || name.isEmpty() || name.split(" ").length < 2) {
+      throw new IllegalArgumentException(
+          "Der Name darf nicht leer sein und muss aus mindestens zwei Teilen bestehen");
+    }
+    this.name = name;
   }
 
   @Override
@@ -29,3 +42,4 @@ public class Patient {
     return number + " " + name;
   }
 }
+
--- src/ueb07/PatientenWarteschlange.java
+++ src/ueb07/PatientenWarteschlange.java
@@ -1,12 +1,16 @@
 package ueb07;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class PatientenWarteschlange {
 
   private Patient[] warteliste;
 
+  /**
+   * Konstruktor Warteschlange Überprüft, ob die eingegebene Größe >= 0 ist
+   *
+   * @param size Maximale Größe der zu erstellenden Wartechlanger
+   */
   public PatientenWarteschlange(int size) {
     if (size <= 0) {
       throw new IllegalArgumentException("Warteschlange muss größer als 0 sein");
@@ -14,6 +18,15 @@ public class PatientenWarteschlange {
     warteliste = new Patient[size];
   }
 
+  /**
+   * Erstellt einen neuen Patienten Überprüft, ob die Warteschlange voll ist oder der Patient
+   * bereits existiert
+   *
+   * @throws IllegalArgumentException Wenn Warteschlange voll ist
+   * @throws IllegalArgumentException Wenn der Patient bereits existiert
+   * @param number ID des zu erstellenden Patienten
+   * @param name Name des zu erstellenden Patienten
+   */
   public void neuerPatient(int number, String name) {
     if (isFull()) {
       throw new IllegalArgumentException("Warteschlange ist voll");
@@ -26,27 +39,52 @@ public class PatientenWarteschlange {
     warteliste[getPatientenAnzahl()] = new Patient(number, name);
   }
 
+  /**
+   * Entfernt einen bestehenden Patienten aus der Warteschlange
+   *
+   * @throws NoSuchElementException Wenn die übergebene ID nicht existiert
+   * @param number die Patientennummer
+   * @return patient
+   */
   public Patient entfernePatient(int number) {
-    for (int i = 0; i < warteliste.length; i++) {
-      if (warteliste[i] != null && warteliste[i].getNumber() == number) {
-        Patient patient = warteliste[i];
-        warteliste[i] = null;
-        return patient;
+    int index = getIndexByNr(number);
+    if (index == -1) {
+      throw new NoSuchElementException(
+          String.format("Der Patient mit der Nummer %d existiert nicht", number));
+    }
+    Patient patient = getPatientByNr(number);
+    for (int i = index; i < warteliste.length; i++) {
+      if (warteliste[i] != null) {
+        warteliste[i] = warteliste[i + 1];
       }
     }
-    throw new NoSuchElementException(
-        String.format("Der Patient mit der Nummer %d existiert nicht", number));
+    if (getPatientenAnzahl() == 0) {
+      System.out.println("Die Warteschlange ist jetzt leer");
+    }
+    return patient;
   }
 
+  /**
+   * Übergibt den Patienten, der als nächstes dran genommen werden soll
+   *
+   * @throws NoSuchElementException Wenn die Warteschlange leer ist
+   * @return der Patient der gerade aufgerufen wurde
+   */
   public Patient derNaechsteBitte() {
-    Patient patient = warteliste[0];
+    final Patient patient = warteliste[0];
     if (patient == null) {
-      return null;
+      throw new NoSuchElementException("Es existiert kein Patient mehr in der Warteschlange");
     }
     entfernePatient(patient.getNumber());
     return patient;
   }
 
+  /**
+   * Sucht anhand der übergebenen ID den Patienten in der Warteschlange
+   *
+   * @param number ID eines Patienten
+   * @return patient oder null
+   */
   public Patient getPatientByNr(int number) {
     for (Patient patient : warteliste) {
       if (patient != null && patient.getNumber() == number) {
@@ -56,25 +94,60 @@ public class PatientenWarteschlange {
     return null;
   }
 
-  public int getPatientenAnzahl() {
-    return (int) Arrays.asList(warteliste).stream().filter(artikel -> artikel != null).count();
+  /**
+   * Ermittelt den Index im Array anhand einer überge
+   *
+   * @param number
+   * @return i oder -1
+   */
+  public int getIndexByNr(int number) {
+    for (int i = 0; i < warteliste.length; i++) {
+      if (warteliste[i] != null && warteliste[i].getNumber() == number) {
+        return i;
+      }
+    }
+    return -1;
   }
 
+  /**
+   * Ermittelt die Anzahl an Patienten in der Warteschlange
+   *
+   * @return count
+   */
+  public int getPatientenAnzahl() {
+    int count = 0;
+    for (Patient patient : warteliste) {
+      if (patient != null) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Übergibt die maximale Größe der Warteschlange
+   *
+   * @return warteliste.length
+   */
   public int getWarteschlagenGroesse() {
     return warteliste.length;
   }
 
+  /** @return Ob die Warteschlange voll ist */
   public boolean isFull() {
     return !((getPatientenAnzahl()) < getWarteschlagenGroesse());
   }
 
+  /** Gibt die ganze Warteschlange aus */
   @Override
   public String toString() {
     final StringBuilder stringBuilder = new StringBuilder();
     stringBuilder.append("Warteliste\n Pnr Name\n");
-    Arrays.asList(warteliste).stream()
-        .filter(patient -> patient != null)
-        .forEach(patient -> stringBuilder.append(patient + "\n"));
+    for (Patient patient : warteliste) {
+      if (patient != null) {
+        stringBuilder.append(patient + "\n");
+      }
+    }
     return stringBuilder.toString();
   }
 }
